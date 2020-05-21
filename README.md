cd19f7693898ff3528cd8a709ae9609fe3a7f11b
Yulia Klyueva <liya-n@list.ru>
2020-05-21 22:43:59 +0300

    ура, получилось

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ajs-hw6_3
 Домашнее задание к лекции «Прототипы, конструкторы» JSDoc
+
+[![Build status](https://ci.appveyor.com/api/projects/status/msb7b29fcktxbo4e/branch/master?svg=true)](https://ci.appveyor.com/project/Mistel-77/ajs-hw6-3/branch/master)
---
+++ src/js/character.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+/**
+ * <Общее описание>
+ *
+ * @param { string } name - name Character
+ * @param { string } type - type Character
+ *
+ * @throws  Will throw an error if the argument is null/undefined
+ */
+function Character(name, type) {
+  this.name = name;
+  this.type = type;
+}
